f5b1005c5a147a2b45d9e8d298f03e3a3ce90fbe
Rusben <rusben@gmail.com>
2016-10-23 22:37:42 +0200

    Backbone structure

---
+++ js/service/converter.js
@@ -0,0 +1,34 @@
+/**
+ * "converter" converts into different formats
+ *
+ * @name: converter
+ * @version: 0.0.1
+ * @author: DAW-M08
+ * @description: Converts into different formats
+ * @params {none}
+ * @return: none
+ */	
+var converter = {
+	version: "0.0.1",
+	version: function () {
+		return this.version;
+	},
+	/**
+	 * @name: decimal2binary
+	 * @description: Converts from decimal to binary
+ 	 * @params {decimal}
+ 	 * @return: the decimal number converted to binary
+	 */
+	decimal2binary: function (decimal) {
+ 		return 0;
+	},
+	/**
+	 * @name: binary2decimal
+	 * @description: Converts from binary to decimal
+ 	 * @params {binary}
+ 	 * @return: the binary number converted to decimal
+	 */
+	binary2decimal: function (binary) {
+ 		return 0;
+	},
+}
---
+++ js/service/parser.js
@@ -0,0 +1,34 @@
+/**
+ * "parser" checks inputs searching for patterns
+ *
+ * @name: parser
+ * @version: 0.0.1
+ * @author: DAW-M08
+ * @description: checks inputs searching for patterns
+ * @params {none}
+ * @return: none
+ */	
+var parser = {
+	version: "0.0.1",
+	version: function () {
+		return this.version;
+	},
+	/**
+	 * @name: isDigit
+	 * @description: Checks if number is between 0-9
+ 	 * @params {number}
+ 	 * @return: true if number is between 0-9, false otherwise
+	 */
+	isDigit: function (number) {
+ 		return false;
+	},
+	/**
+	 * @name: isBit
+	 * @description: Checks if number is 0 or 1
+ 	 * @params {number}
+ 	 * @return: true if number is 0 or 1, false otherwise
+	 */
+	isBit: function (number) {
+ 		return false;
+	},
+}
